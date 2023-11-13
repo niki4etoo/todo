6257caf32fcec44102a57bdebd5992e4728da9fd
Nikolai Nanev <niki4etooo@gmail.com>
2023-11-13 12:03:08 +0200

    added keydown enter when adding new task

--- src/App.tsx
+++ src/App.tsx
@@ -11,19 +11,19 @@ function App() {
 
   const taskInputRef = useRef(null);
 
-  const [ showList, setShowList ] = useState({ showList: true, newItem: false, removedItem: false });
+  const [showList, setShowList] = useState({ showList: true, newItem: false, removedItem: false });
 
   const addTask = (target: any | null) => {
-    if(target !== null){
+    if (target !== null) {
       list.add(target.value);
-      setShowList((state) => { return { ... state, newItem: true}});
+      setShowList((state) => { return { ...state, newItem: true } });
       target.value = "";
     }
   }
 
   const removeTask = () => {
     list.getList().shift();
-    setShowList((state) => { return { ... state, removedItem: true}})
+    setShowList((state) => { return { ...state, removedItem: true } })
   }
 
 
@@ -34,7 +34,7 @@ function App() {
       <hr className="my-12 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-neutral-800 to-transparent opacity-25 dark:opacity-100" />
       <div className='flex justify-center align-middle flex-col'>
         {
-          showList && 
+          showList &&
           list.getList().map((task) => (
             <div className='flex-item' key={uuid4()}>{task}</div>
           ))
@@ -46,12 +46,17 @@ function App() {
       focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
       disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
       invalid:border-pink-500 invalid:text-pink-600
-      focus:invalid:border-pink-500 focus:invalid:ring-pink-500' name='task' type='text' />
+      focus:invalid:border-pink-500 focus:invalid:ring-pink-500' name='task' type='text' onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                addTask(taskInputRef.current);
+              }
+
+            }} />
         </div>
       </div>
       <div className='flex'>
         <div className='flex-initial w-64'>
-          <button className='hover:text-opacity-80 hover:border-slate-800' onClick={() => {            
+          <button className='hover:text-opacity-80 hover:border-slate-800' onClick={() => {
             addTask(taskInputRef.current);
           }}>
             Add
